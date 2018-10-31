a368a43f4745b0936db94bbdbbd40da140e8f094
john <john@john-PC>
2018-10-31 14:39:01 +0800

    jj

--- RR/src/com/baidu/HH.java
+++ RR/src/com/baidu/HH.java
@@ -4,6 +4,7 @@ public class HH {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 2;
 		System.out.println(3);
 		System.out.println(3);
 		System.out.println(3);
