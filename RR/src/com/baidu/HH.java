cf7e9b5decf0e2d9bba8033a559570eea9c1d692
john <john@john-PC>
2018-10-31 13:24:02 +0800

    cc

--- RR/src/com/baidu/HH.java
+++ RR/src/com/baidu/HH.java
@@ -1,10 +1,10 @@
 package com.baidu;
 
-public class HelloWorld {
+public class HH {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println(1);
 
 	}
 
